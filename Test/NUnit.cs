4002eaecd42b60cc47afe7f55987956f5ebe3e47
d e e <d_vine1@hotmail.com>
2018-02-04 12:14:40 -0700

    added driver, iwebelement, testing assert true statement.

---
+++ Test/NUnit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Test
+{
+    class NUnit
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void InitializeBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestNunit()
+        {
+            driver.Navigate().GoToUrl("https://worldofwarcraft.com/en-us/");
+            driver.Manage().Window.Maximize();
+
+            IWebElement testing = driver.FindElement(By.XPath("Logo"));
+            IWebElement testing = driver.FindElement()
+            Assert.IsTrue(testing.Displayed);
+
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+        
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver webdriver = new ChromeDriver();
+            webdriver.Navigate().GoToUrl("https://www.google.com");
+        }
+    }
+}
